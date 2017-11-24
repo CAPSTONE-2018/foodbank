db2db0c87c03cc491ad2e1a997dae62cf23acf4d
Cindy Howard <howardcy@lewisu.edu>
2017-11-24 22:15:34 +0000

    one more conflict

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -3,12 +3,9 @@ class FormsController < ApplicationController
     def index
         @forms = Form.all
     end
-<<<<<<< HEAD
-=======
     
     def new
         
     end
     
->>>>>>> e8e4e74393da09d5fd3ed6a80f97685d7cb2bc98
 end
