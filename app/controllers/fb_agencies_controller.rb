f5a8d736b318f7416fba992e826a91373e1f56de
Ivan Fonseca <ivanfonseca@lewisu.edu>
2017-12-10 23:38:54 +0000

    Fb Agency Edit

--- app/controllers/fb_agencies_controller.rb
+++ app/controllers/fb_agencies_controller.rb
@@ -1,70 +1,34 @@
 class FbAgenciesController < ApplicationController
-before_action :load_fbagency
-
-
-  def index
-    @fb_agencies = FbAgency.all
-  end
-
-  def show
-  end
-
-  def new
-    @wizard = ModelWizard.new(FbAgency, session, params).start
-    @fb_agency = @wizard.object
-    logger.debug "New agency: #{@fb_agency.attributes.inspect}"
-  end
-
-  def edit
-    @wizard = ModelWizard.new(@fb_agencies, session, params).start
-  end
-
-  def create
-    @wizard = ModelWizard.new(FbAgency, session, params, agencies_params).continue
-    @fb_agency = @wizard.object
-    if @wizard.save
-      @fb_agency = FbAgency.create!(agencies_params)
-        session[:fbagency_attributes] = nil
-        render '/forms/groceryplan'
-    else
-      render :new
-    end
-  end
-
-  def update
-    @wizard = ModelWizard.new(@fb_agencies, session, params, agencies_params).continue
-    if @wizard.save
-      redirect_to fb_agencies, notice: 'updated.'
-    else
-      render :edit
-    end
-  end
-  
-def destroy
-      @fb_agencies = FbAgency.find(params[:id])
-      @fb_agencies.destroy
-      flash[:notice] = "Agency '#{@fb_agencies.AgencyName}' deleted."
-      redirect_to fb_agencies_path
-end
-
-
-
-private
-
-  def load_fbagency
-    @fb_agencies = FbAgency.find_by(id: params[:id])
-  end
-
-  def agencies_params
-    return params unless params[:fb_agency]
-
-    params.require(:fb_agency).permit(:current_step,
-      :AgencyName, :AgencyCounty, :AgencyNumber, :TodaysDate, :DofLastMonitor, :ParentAgency,
+    
+    def agencies_params
+        params.require(:fb_agency).permit(:AgencyName, :AgencyCounty, :AgencyNumber, :TodaysDate, :DofLastMonitor, :ParentAgency,
         :PAgencyNum, :SiteAddress, :SiteNumber, :ContactName, :ContactNum, :Director, :DirectorNum, :AdditionalContact, :AdditionalNum, 
-        :PrimaryEmail, :DateMostRecentTraining, :TaxExemptOnFile, :IRSVerification, :DateOfVerification, :EEP,
-        :DateOfContract, :SoupKitchen, :GroupHome, :Shelter, :Daycare, :YouthProgram, :FoodPantry, :Seasonal, :MobilePantry, :Backpack, :PersonInterviewed, :DaysOfOperation, :SingleAudit, :HandicapAccessible => [:id, :name]
-    )
-  end
-
+        :PrimaryEmail, :DateMostRecentTraining, :TaxExemptOnFile, :IRSVerification, :DateOfVerification, :EEP, :DateOfContract,
+        :SoupKitchen, :GroupHome, :Shelter, :Daycare, :YouthProgram, :FoodPantry, :Seasonal, :MobilePantry, :Backpack, :PersonInterviewed, :DaysOfOperation, :SingleAudit, :HandicapAccessible)
+    end
+ 
+    def index
+        @fb_agencies = FbAgency.all
+    end
+ 
+    def create
+        @fb_agency = FbAgency.create!(agencies_params)
+        render '/forms/mealplan'
+    end 
+    def edit
+        @fb_agency = FbAgency.find(params[:id])
+    end
+    def update
+        @fb_agency = FbAgency.find params[:id]
+        @fb_agency.update_attributes!(agencies_params)
+        flash[:notice] = "#{@fb_agency.AgencyName} was successfully updated."
+        redirect_to fb_agencies_path
+    end
+    def destroy
+        @fb_agency = FbAgency.find(params[:id])
+        @fb_agency.destroy
+        flash[:notice] = "Agency '#{@fb_agency.AgencyName}' deleted."
+        redirect_to fb_agencies_path
+    end
+    
 end
-
