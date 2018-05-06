feeb254b62381852a1d8b34f5b452d18b1c91365
David Gagnon <davidagagnon@lewisu.edu>
2018-05-06 04:55:27 +0000

    added main site, as well as only lewis emails can create an account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
     has_secure_password 
     validates :first_name, :last_name, :email, presence: true
+    validates :email, format: {
+      with: %r{lewisu\.edu}i,
+      message: 'Email must be a student email'
+  }
     validates :password, length: {minimum: 4 }
     validates :password, length: {maximum: 14 }
     validates :email, uniqueness: true
